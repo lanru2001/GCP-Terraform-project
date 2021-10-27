cfc164dc5c03563e9ba03a2ca7085376e8810e5c
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2021-10-27 18:40:39 -0500

    Create network.tf

---
+++ compute-engine/network.tf
@@ -0,0 +1,22 @@
+resource "google_compute_network" "vpc_network" {
+  project                 = var.project
+  name                    = var.network_name 
+  auto_create_subnetworks = true
+  mtu                     = 1460 # maximum transmission units in byte
+}
+
+
+#Google network firewall 
+resource "google_compute_firewall"   "rules_firewall" {
+  project                 = var.project
+  name                    = var.firewall_name
+  network                 = var.network
+  description             = "Creates firewall rule targeting tagged instances"
+
+  allow {
+    protocol              = var.protocol 
+    ports                 = var.ports
+  }
+  source_ranges           = var.source_ranges
+  target_tags             = var.target_tags
+}
