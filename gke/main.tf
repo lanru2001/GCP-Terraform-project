1252082f7356a0a2810f707ad79c19a453587815
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2021-10-28 14:20:59 -0500

    Update main.tf

--- gke/main.tf
+++ gke/main.tf
@@ -97,15 +97,6 @@ resource "google_container_node_pool" "node_pool" {
     delete = "30m"
   }
 }
-
-# CREATE A CUSTOM SERVICE ACCOUNT TO USE WITH THE GKE CLUSTER
-module "gke_service_account" {
-  source = "git::git@github.com:gruntwork-io/terraform-google-gke.git//modules/gke-service-account?ref=v0.3.8"
-
-  name        = var.cluster_service_account_name
-  project     = var.project
-  description = var.cluster_service_account_description
-}
  
 # ALLOW THE CUSTOM SERVICE ACCOUNT TO PULL IMAGES FROM THE GCR REPO
 resource "google_storage_bucket_iam_member" "member" {
