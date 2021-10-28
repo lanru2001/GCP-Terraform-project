d64064829381bec9b07ad89171921d92965bd3cd
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2021-10-28 16:51:09 -0500

    Update main.tf

--- gke/main.tf
+++ gke/main.tf
@@ -1,126 +1,47 @@
-
-module "gke_cluster" {
-  # Use a version of the gke-cluster module that supports Terraform 0.12
-  source = "git::git@github.com:gruntwork-io/terraform-google-gke.git//modules/gke-cluster?ref=v0.3.8"
-
-  name = var.cluster_name
-
-  project  = var.project
-  location = var.location
-  network  = module.vpc_network.network
-
-  # We're deploying the cluster in the 'public' subnetwork to allow outbound internet access
-  subnetwork = module.vpc_network.public_subnetwork
-
-  # When creating a private cluster, the 'master_ipv4_cidr_block' has to be defined and the size must be /28
-  master_ipv4_cidr_block = var.master_ipv4_cidr_block
-
-  # This setting will make the cluster private
-  enable_private_nodes = "true"
-
-  # To make testing easier, we keep the public endpoint available. In production, we highly recommend restricting access to only within the network boundary, requiring your users to use a bastion host or VPN.
-  disable_public_endpoint = "false"
-
-  # With a private cluster, it is highly recommended to restrict access to the cluster master
-  # However, for testing purposes we will allow all inbound traffic.
-  master_authorized_networks_config = [
-    {
-      cidr_blocks = [
-        {
-          cidr_block   = "0.0.0.0/0"
-          display_name = "all-for-testing"
-        },
-      ]
-    },
-  ]
-
-  cluster_secondary_range_name = module.vpc_network.public_subnetwork_secondary_range_name
+resource "google_container_cluster" "primary" {
+  
+  name                     = "my-gke-cluster"
+  location                 = "us-central1"
+  project                  = var.project  
+  network                  = var.network
+  subnetwork               = var.subnetwork
+  description              = "Application cluster for container orchestrated app"
+  #default_max_pods_per_node = 
+  #enable_tpu              =
+  enable_autopilot         = false
+  
+  #Adding this block enables IP aliasing, making the cluster VPC-native instead of routes-based
+  #ip_allocation_policy    =
+  enable_legacy_abac       = false
+  remove_default_node_pool = true
+  initial_node_count       = 3
+  cluster_ipv4_cidr        = "10.96.0.0/14"
+  
+  #cluster_autoscaling     = 
+  networking_mode          = "ROUTES"
+  machine_type             = "e2-medium"
+  #logging_config          =
+  logging_service          = "logging.googleapis.com/kubernetes"
+  #labels
+  min_master_version
 }
-    
-# CREATE A NODE POOL
 
-resource "google_container_node_pool" "node_pool" {
-  provider = google-beta
 
-  name     = "private-pool"
-  project  = var.project
-  location = var.location
-  cluster  = module.gke_cluster.name
-
-  initial_node_count = "1"
-
-  autoscaling {
-    min_node_count = "1"
-    max_node_count = "5"
-  }
-
-  management {
-    auto_repair  = "true"
-    auto_upgrade = "true"
-  }
-
+resource "google_container_node_pool" "primary_preemptible_nodes" {
+  
+  name           = "my-node-pool"
+  cluster        = google_container_cluster.primary.id
+  node_count     = 1
+  node_locations = var.zone
+  project        = var.project  
   node_config {
-    image_type   = "COS"
-    machine_type = "n1-standard-1"
+    preemptible  = true
+    machine_type = "e2-medium"
 
-    labels = {
-      private-pools-example = "true"
-    }
-
-    # Add a private tag to the instances. See the network access tier table for full details:
-    # https://github.com/gruntwork-io/terraform-google-network/tree/master/modules/vpc-network#access-tier
-    tags = [
-      module.vpc_network.private,
-      "private-pool-example",
-    ]
-
-    disk_size_gb = "30"
-    disk_type    = "pd-standard"
-    preemptible  = false
-
-    service_account = module.gke_service_account.email
-
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    service_account = google_service_account.default.email
     oauth_scopes = [
-      "https://www.googleapis.com/auth/cloud-platform",
+      "https://www.googleapis.com/auth/cloud-platform"
     ]
   }
-
-  lifecycle {
-    ignore_changes = [initial_node_count]
-  }
-
-  timeouts {
-    create = "30m"
-    update = "30m"
-    delete = "30m"
-  }
 }
- 
-# ALLOW THE CUSTOM SERVICE ACCOUNT TO PULL IMAGES FROM THE GCR REPO
-#resource "google_storage_bucket_iam_member" "member" {
-#  bucket = "artifacts.${var.project}.appspot.com"
-#  role   = "roles/storage.objectViewer"
-#  member = "serviceAccount:${module.gke_service_account.email}"
-#}
-  
-# CREATE A NETWORK TO DEPLOY THE CLUSTER TO
-module "vpc_network" {
-  source = "github.com/gruntwork-io/terraform-google-network.git//modules/vpc-network?ref=v0.2.1"
-
-  name_prefix = "${var.cluster_name}-network-${random_string.suffix.result}"
-  project     = var.project
-  region      = var.region
-
-  cidr_block           = var.vpc_cidr_block
-  secondary_cidr_block = var.vpc_secondary_cidr_block
-}
-
-# Use a random suffix to prevent overlap in network names
-resource "random_string" "suffix" {
-  length  = 4
-  special = false
-  upper   = false
-}
-
-  
-  
