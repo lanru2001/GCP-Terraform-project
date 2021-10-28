73c9d99f51033fae1689ae044c77582ca31d369f
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2021-10-28 17:07:51 -0500

    Delete service_account.tf

--- gke/service_account.tf
+++
@@ -1,8 +0,0 @@
-# CREATE A CUSTOM SERVICE ACCOUNT TO USE WITH THE GKE CLUSTER
-module "gke_service_account" {
-  source = "git::git@github.com:gruntwork-io/terraform-google-gke.git//modules/gke-service-account?ref=v0.3.8"
-
-  name        = var.cluster_service_account_name
-  project     = var.project
-  description = var.cluster_service_account_description
-}
