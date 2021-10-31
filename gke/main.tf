d18ccbf2135fe46db101f18ec33ca2551aa772d8
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2021-10-31 11:49:13 -0500

    Update main.tf

--- gke/main.tf
+++ gke/main.tf
@@ -6,36 +6,38 @@ resource "google_container_cluster"  "my_cluster" {
   network                    = var.network
   subnetwork                 = var.subnetwork
   description                = "Application cluster for container orchestrated app"
-  #default_max_pods_per_node = 
-  #enable_tpu                =
-  #enable_autopilot          = false
+ 
+  ip_range_pods             = var.ip_range_pods
+  ip_range_services         = var.ip_range_services  
+  
+  # optional variables
+  kubernetes_version        = "1.16.11-gke.5"
+  regional                  = true
+  create_service_account    = false
+  remove_default_node_pool  = true
   
   #Adding this block enables IP aliasing, making the cluster VPC-native instead of routes-based
-  #ip_allocation_policy      =
   enable_legacy_abac         = false
   remove_default_node_pool   = true
   initial_node_count         = 3
   cluster_ipv4_cidr          = "10.96.0.0/14"
   
+  # addons
+  network_policy             = false
+  horizontal_pod_autoscaling = true
+  http_load_balancing        = true  
    #master_auth {
    #        client_certificate     = 
    #        client_key             = 
    #        cluster_ca_certificate = 
    #        password               = 
    #        username               =  
-  
-  #cluster_autoscaling       = 
+ 
   networking_mode            = "ROUTES"
-
-  #logging_config            =
+  
   logging_service            = "logging.googleapis.com/kubernetes"
-  #labels
-  #min_master_version
-  #node_locations = [
-  #  "us-central1-c",
-  #]
+  
 }
-
 
 resource "google_container_node_pool"    "my_cluster_node_pool" {
   
@@ -47,23 +49,31 @@ resource "google_container_node_pool"    "my_cluster_node_pool" {
   node_config   {
     preemptible        = true
     machine_type       = "e2-medium"
-    #disk_size_gb      = 
-    #disk_type         = 
-    #guest_accelerator = 
-    #image_type        = 
+    disk_size_gb       = 20
+    disk_type          = "pd-standard"
+    image_type         = "COS"
     #labels            = 
-    #local_ssd_count   = 
+    local_ssd_count    = 0
     #metadata          = 
-    # service_account   =
-    #shielded_instance_config {
-    #           enable_integrity_monitoring = (known after apply)
-    #           enable_secure_boot          = (known after apply)
-    #        }
     
     # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
     #service_account = "google_service_account.446991545978-compute@developer.gserviceaccount.com"
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/cloud-platform"
-    ]
+    #oauth_scopes = [
+    #  "https://www.googleapis.com/auth/cloud-platform"
+    #]
+    
+    node_pools_oauth_scopes = {
+       all = []
+       default-node-pool = [
+         "https://www.googleapis.com/auth/devstorage.read_only",
+         "https://www.googleapis.com/auth/logging.write",
+         "https://www.googleapis.com/auth/monitoring",
+         "https://www.googleapis.com/auth/ndev.clouddns.readwrite",
+         "https://www.googleapis.com/auth/service.management.readonly",
+         "https://www.googleapis.com/auth/servicecontrol",
+         "https://www.googleapis.com/auth/trace.append",
+         "https://www.googleapis.com/auth/cloud-platform",       ]
+    }
+    
   }
 }
